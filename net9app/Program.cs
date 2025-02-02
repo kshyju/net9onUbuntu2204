a7231fbbe7a096dec9dc684575710992c4affcf9
Shyju Krishnankutty <connectshyju@gmail.com>
2025-02-02 07:23:06 -0800

    add 2 .net9 test apps.

---
+++ net9app/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (var i = 0; i < 10; i++)
+{
+    Console.WriteLine($"The current value of i is {i}");
+}
---
+++ net9func/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Build().Run();
